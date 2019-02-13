b46110ec1d87db1a93fdcfc867b81ba3a465b86c
wesley <cnwesleywang@gmail.com>
2019-02-13 11:10:16 +0800

    fix tablename should be safe

--- mock/context.js
+++ mock/context.js
@@ -16,8 +16,8 @@ ctx.db.init=async function(){
     const rows = await mysql.query(`SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = '${mysql._client.config.database}'`);
     const tables=[];
     for (let row of rows){
-        const create = await mysql.query(`show create table ${row.table_name}`);
-        const truncate = `truncate table ${row.table_name}`;
+        const create = await mysql.query(`show create table \`${row.table_name}\``);
+        const truncate = `truncate table \`${row.table_name}\``;
         tables.push({create:create[0]['Create Table'], truncate});
     }
     await mysql.query(`create database ${mockdbname}`);
