1865fe58ad3ec672f93cc02a28e43373dc328a85
wesleywang <9610848@qq.com>
2019-09-19 21:16:42 +0800

    smart gateway route

--- gateway/index.js
+++ gateway/index.js
@@ -4,6 +4,39 @@ const task = require('..')('gateway', {
 });
 
 const proxy = require('koa-server-http-proxy');
+const koaConnect = require('koa2-connect')
+const httpProxy = require('http-proxy-middleware')
+
+const routemap = (task.config.gateway.services || []).reduce((r,v) => {r[v]=1; return r;}, {})
+const proxymap = {}
+async function normalroute(ctx, next) {
+    if (!(ctx.request.path in routemap)) {
+        if (!(ctx.request.path in proxymap)) {
+            const servicename = ctx.request.path.split('/').splice(1,1).pop()
+            if (servicename && task.config[servicename] && task.config[servicename].port) {
+                proxymap[ctx.request.path] = httpProxy(ctx.request.path, {
+                    target: `http://127.0.0.1:${task.config[servicename].port}`, 
+                    changeOrigin: true,
+                    onError: (err, req, res)=>{
+                        res.writeHead(501, {
+                            'Content-Type': 'text/plain'
+                        })
+                        res.end(
+                            'Something went wrong. And we are reporting a custom error message.'
+                        )
+                    }    
+                })    
+            }
+        }
+        if (ctx.request.path in proxymap) {
+            await koaConnect(proxymap[ctx.request.path])(ctx, next)
+        } else {
+            ctx.status = 404
+        }
+    } else {
+        await next()
+    }
+}
 
 const eldstart = task.start;
 task.start = ((addr)=>{
@@ -21,6 +54,7 @@ task.start = ((addr)=>{
             },
         }));
     })
+    task.api.use(normalroute)
     eldstart(addr);
 }).bind(task);
 
