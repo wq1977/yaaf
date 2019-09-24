1df0eb2cacdd56d38945bf8d291375dc4bc0c64a
wesleywang <9610848@qq.com>
2019-09-24 18:40:05 +0800

    gradation support

--- gateway/index.js
+++ gateway/index.js
@@ -3,19 +3,20 @@ const task = require('..')('gateway', {
     parseBody: false
 });
 
-const proxy = require('koa-server-http-proxy');
 const koaConnect = require('koa2-connect')
 const httpProxy = require('http-proxy-middleware')
 
-const routemap = (task.config.gateway.services || []).reduce((r,v) => {r[v]=1; return r;}, {})
+const serviceMap = {}
 const proxymap = {}
 async function normalroute(ctx, next) {
-    if (!(ctx.request.path in routemap)) {
-        if (!(ctx.request.path in proxymap)) {
+    const host = ctx.sessionData.gradation && ctx.config.gradation ? ctx.config.gradation.host : '127.0.0.1'
+    const key = `${host}${ctx.request.path}`
+    if (!(key in serviceMap)) {
+        if (!(key in proxymap)) {
             const servicename = ctx.request.path.split('/').splice(1,1).pop()
             if (servicename && task.config[servicename] && task.config[servicename].port) {
                 proxymap[ctx.request.path] = httpProxy(ctx.request.path, {
-                    target: `http://127.0.0.1:${task.config[servicename].port}`, 
+                    target: `http://${host}:${task.config[servicename].port}`, 
                     changeOrigin: true,
                     onError: (err, req, res)=>{
                         res.writeHead(501, {
@@ -28,8 +29,8 @@ async function normalroute(ctx, next) {
                 })    
             }
         }
-        if (ctx.request.path in proxymap) {
-            await koaConnect(proxymap[ctx.request.path])(ctx, next)
+        if (key in proxymap) {
+            await koaConnect(proxymap[key])(ctx, next)
         } else {
             ctx.status = 404
         }
@@ -40,19 +41,26 @@ async function normalroute(ctx, next) {
 
 const eldstart = task.start;
 task.start = ((addr)=>{
-    (task.config.gateway.services || []).map((service)=>{
-        task.api.use(proxy(service[0], { 
-            target: service[1], 
-            changeOrigin: true,
-            onError: (err, req, res)=>{
-                res.writeHead(501, {
-                    'Content-Type': 'text/plain'
-                })
-                res.end(
-                    'Something went wrong. And we are reporting a custom error message.'
-                )
-            },
-        }));
+    task.api.use(async (ctx, next) => {
+        const host = ctx.sessionData.gradation && ctx.config.gradation ? ctx.config.gradation.host : '127.0.0.1'
+        const key = `${host}${ctx.request.path}`
+        if (!(key in serviceMap)) {
+            serviceMap[key] = httpProxy(ctx.request.path, {
+                target: ctx.config.gradation ? service[1].replace(/[\d.]+/, host) : service[1], 
+                changeOrigin: true,
+                onError: (err, req, res)=>{
+                    res.writeHead(501, {
+                        'Content-Type': 'text/plain'
+                    })
+                    res.end(
+                        'Something went wrong. And we are reporting a custom error message.'
+                    )
+                },
+            })    
+        }
+        if (key in serviceMap) {
+            await koaConnect(serviceMap[ctx.request.path])(ctx, next)
+        } else await next()
     })
     task.api.use(normalroute)
     eldstart(addr);
